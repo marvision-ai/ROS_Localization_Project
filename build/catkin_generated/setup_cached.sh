c337eb0a8bc5801678f114daec1854531b0fcfc6
Martin Bufi <mbufi1992@gmail.com>
2018-02-07 16:56:29 -0800

    workspace initialization

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/robond/ROS_Localization_Project/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/robond/ROS_Localization_Project/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/robond/ROS_Localization_Project/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/robond/ROS_Localization_Project/src:$ROS_PACKAGE_PATH"
