251d1b1a7d96badcf09e75383925996ff0947b9c
Martin Bufi <mbufi1992@gmail.com>
2018-02-18 13:57:18 -0800

    basic URDF and world files for martin_bot

---
+++ build/martin_bot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/ROS_Localization_Project/src/martin_bot
+# Build directory: /home/robond/ROS_Localization_Project/build/martin_bot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
